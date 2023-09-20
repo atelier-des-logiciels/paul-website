28e94dcc5228b49a84178fcd6f986fbca8ae68ee
Paul Musso <paul@musso.io>
2023-09-21 01:23:40 +0200

    chore: test update

--- website/src/App/index.tsx
+++ website/src/App/index.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-      <h1 className="text-4xl text-blue-700">My Webpack + Tailwind App</h1>
+      <h1 className="text-4xl text-blue-700">Paul website</h1>
     </div>
   );
 }
