35cce8e5b2e2f68b49e7d936481d4f0fc6738e81
Paul Musso <paul@musso.io>
2023-09-26 22:51:39 +0200

    chore: <Footer />

---
+++ website/src/App/Footer/Footer.tsx
@@ -0,0 +1,38 @@
+function Footer() {
+  return (
+    <div className="bg-primary-900 bottom-0 w-full text-white p-6 flex justify-center gap-24">
+      <div className="flex gap-12 flex-initial w-1/6 lg:w-2/12">
+        <div className="flex flex-col gap-4">
+          <h3 className="font-bold" id="contact">
+            Contactez-moi
+          </h3>
+          <div>
+            <p>10 rue Peyresourde</p>
+            <p>31880 La Salvetat Saint Gilles</p>
+          </div>
+          <div>
+            <p>Téléphone:</p>
+            <a href="tel:0613281158">06 13 28 11 58</a>
+          </div>
+          <div>
+            <p>Email:</p>
+            <a href="mailto:paul@musso.io">paul@musso.io</a>
+          </div>
+        </div>
+      </div>
+      <div className="flex-initial w-5/6 md:w-3/6 lg:w-5/12">
+        <div className="flex flex-col gap-4">
+          <h3 className="font-bold">Navigation</h3>
+          <div className="flex gap-2 flex-col">
+          <a href="/services">Mes services</a>
+          <a href="/values">Mes valeurs</a>
+          <a href="/clients">Mes clients</a>
+          <a href="/cv">Mon parcours</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- website/src/App/index.tsx
+++ website/src/App/index.tsx
@@ -1,11 +1,13 @@
+import Footer from './Footer';
 import Header from './Header';
 import WelcomePage from './WelcomePage';
 
 function App() {
   return (
-    <div className="font-sans">
+    <div className="font-sans min-h-screen flex justify-between flex-col">
       <Header />
       <WelcomePage />
+      <Footer />
     </div>
   );
 }
