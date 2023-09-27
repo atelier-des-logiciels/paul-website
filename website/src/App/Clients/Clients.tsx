ca48e9d8de080051ca5ab72c2031da44ce9d2381
Paul Musso <paul@musso.io>
2023-09-28 00:29:18 +0200

    chore: add client links

--- website/src/App/Clients/Clients.tsx
+++ website/src/App/Clients/Clients.tsx
@@ -9,37 +9,101 @@ import dappy from './images/dappy.png';
 
 function Clients() {
   return (
-    <div className="flex flex-col pb-24">
+    <div className="flex flex-col pb-24 pt-12">
       <h3 className="text-center text-2xl font-semibold pb-12">Mes clients</h3>
       <div className="flex items-center w-5/6 flex-wrap self-center justify-center gap-12">
-        <img
-          src={airbus}
-          alt="airbus logo"
-          className="w-64 inline-block pr-4"
-        />
-        <img src={cnes} alt="cnes logo" className="w-64 inline-block pr-4" />
-        <img
-          src={orange}
-          alt="orange logo"
-          className="w-64 inline-block pr-4"
-        />
-        <img
-          src={ecocert}
-          alt="ecocert logo"
-          className="w-64 inline-block pr-4"
-        />
-        <img src={allocine} alt="allocine logo" className="w-64 inline-block" />
-        <img
-          src={bravas}
-          alt="bravas logo"
-          className="w-64 inline-block pr-4"
-        />
-        <img src={dappy} alt="dappy logo" className="w-48 inline-block pr-4" />
-        <img
-          src={boostheat}
-          alt="boostheat logo"
-          className="w-64 inline-block pr-4"
-        />
+        <a
+          href="https://www.airbus.com/"
+          title="site web d'Airbus"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={airbus}
+            alt="airbus logo"
+            className="w-64 inline-block pr-4"
+          />
+        </a>
+        <a
+          href="https://www.cnes.fr"
+          title="site web du CNES"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={cnes} alt="cnes logo" className="w-64 inline-block pr-4" />
+        </a>
+        <a
+          href="https://www.orange.fr"
+          title="site web d'Orange"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={orange}
+            alt="orange logo"
+            className="w-64 inline-block pr-4"
+          />
+        </a>
+        <a
+          href="https://www.ecocert.com"
+          title="site web d'Ecocert"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={ecocert}
+            alt="ecocert logo"
+            className="w-64 inline-block pr-4"
+          />
+        </a>
+        <a
+          href="https://www.allocine.fr"
+          title="site web d'Allociné"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={allocine}
+            alt="allocine logo"
+            className="w-64 inline-block"
+          />
+        </a>
+        <a
+          href="https://www.bravas.io"
+          title="site web de Bravas"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={bravas}
+            alt="bravas logo"
+            className="w-64 inline-block pr-4"
+          />
+        </a>
+        <a
+          href="https://dappy.tech"
+          title="site web de Dappy"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={dappy}
+            alt="dappy logo"
+            className="w-48 inline-block pr-4"
+          />
+        </a>
+        <a
+          href="https://boostheat.fr"
+          title="site web de Boostheat"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={boostheat}
+            alt="boostheat logo"
+            className="w-64 inline-block pr-4"
+          />
+        </a>
       </div>
     </div>
   );
